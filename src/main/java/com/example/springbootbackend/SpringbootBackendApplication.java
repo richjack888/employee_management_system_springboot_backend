8ed7a54c2148acd59cf964c932236c487ce52549
richjack888 <angel800114@gmail.com>
2022-06-17 22:01:27 +0800

    add Employee and EmployeeRepository

--- src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
+++ src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
@@ -1,9 +1,11 @@
 package com.example.springbootbackend;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class SpringbootBackendApplication {
 
     public static void main(String[] args) {
