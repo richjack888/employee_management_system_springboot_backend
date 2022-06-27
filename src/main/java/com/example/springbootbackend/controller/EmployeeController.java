d7804f80abedf97aa781f5db1c5d68389b3cf5d6
richjack888 <angel800114@gmail.com>
2022-06-27 20:45:41 +0800

    create get employee by id version 2

--- src/main/java/com/example/springbootbackend/controller/EmployeeController.java
+++ src/main/java/com/example/springbootbackend/controller/EmployeeController.java
@@ -4,7 +4,6 @@ import com.example.springbootbackend.exception.ResourceNotFoundException;
 import com.example.springbootbackend.model.Employee;
 import com.example.springbootbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,14 +30,22 @@ public class EmployeeController {
     // creat employee
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee) {
-        return employeeRepository.save(employee);
-    }
+            return employeeRepository.save(employee);
+        }
 
-    // read employee by id
+//    // read employee by id
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+//        Employee employee = employeeRepository.findById(id)
+//                .orElseThrow(() -> new ResourceNotFoundException("Employee ID not found:" + id));
+//        return ResponseEntity.ok(employee);
+//    }
+
+    // read employee by id - Version 2
     @GetMapping("/{id}")
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
-        Employee employee = employeeRepository.findById(id)
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee ID not found:" + id));
-        return ResponseEntity.ok(employee);
+
     }
 }
